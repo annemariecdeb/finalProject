3c74b5f61d9b4c2b07bf3c3de4d7e0f1e0056e30
annemariecdeb <annemariecdeb@gmail.com>
2016-12-19 01:08:21 -0500

    Updated CSS, Index and JS to reflect grid on marketing page.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+$('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+  columnWidth: '.grid-sizer'
+  }
+});
+
+
+
+
  $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
